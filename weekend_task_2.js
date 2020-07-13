1d455d15517dadf50e021915080eba5ee743b519
fikri <me@fikri.tech>
2020-07-13 11:28:52 +0700

    function

--- weekend_task_2.js
+++ weekend_task_2.js
@@ -99,3 +99,85 @@ waves('abc') // return Abc aBc abC
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+function findShortest(arr){
+    var min = arr[0] // budi
+
+    for(var i = 1 ; i <arr.length;i++){
+        if(arr[i].length < min.length){
+            min = arr[i]
+        }
+    }
+    console.log(min)
+}
+findShortest(['budi','pergi','ke']) // ke
+findShortest(['aku','cinta','purwadhika']) // aku
+
+
+function checkExam (kunci,jawaban){
+   var totalBenar = 0
+//    if(kunci[0] == jawaban[0]){
+//         totalBenar ++
+//    }
+//    if(kunci[1] == jawaban[1]){
+//        totalBenar ++
+//    }
+//    if(kunci[2] == jawaban[2]){
+//        totalBenar ++
+//    }
+//    if(kunci[3] == jawaban[3]){
+//        totalBenar ++
+//    }
+
+    for(var i = 0 ; i< kunci.length ; i ++){
+        if(kunci[i] == jawaban[i]){
+            totalBenar ++
+        }
+    }
+   console.log(totalBenar * 3)
+}
+
+checkExam(["a", "a", "b", "b","c"], ["a", "c", "b", "d","c"]) // bener * 3 = 9
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) // bener * 3 = 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "a",  "a"]) // bener * 3 = 6
+
+
+
+
+//********* 1 isDivisible(num,div1,div2) 
+// Accepts Three Parameters
+
+function isDivisible(num,div1,div2){
+    // modulus %
+    if(((num % div1) == 0) && ((num % div2) == 0)){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
+
+
+isDivisible(100,5,2) // true
+isDivisible(10,3,1) // false
+isDivisible(12,3,4) // true
+
+
+// ******** 2 monthConverter(num)
+function monthConverter(num){
+    var dataMonth = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Agus','Sept','Okt','Nov','Des']
+    console.log(dataMonth[num-1])
+}
+
+monthConverter(4) // April
+
+
