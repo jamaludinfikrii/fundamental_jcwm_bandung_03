9bb0dbe86b7f47691783804bef8dd4311f97453d
fikri <me@fikri.tech>
2020-07-14 10:10:32 +0700

    crud object

--- day_9/object.js
+++ day_9/object.js
@@ -6,7 +6,6 @@
 
 
 
-
 // ********* CREATE
 // Kenapa mesti pake object
 // ada data siswa, namanya budi, umurnya 30, jenis kelamin lelaki
@@ -17,6 +16,15 @@ var jenisKelamin = 'lelaki'
 
 // Object Literal
 var siswa = {
+    nama : "budi",
+    umur : 30,
+    jenisKelamin : 'lelaki',
+    printData : function(){
+        console.log(this.nama + ' ' + this.umur)
+    }
+}
+
+var siswa2 = {
     nama : "budi",
     umur : 30,
     jenisKelamin : 'lelaki'
@@ -32,16 +40,6 @@ siswa2.jenisKelamin = 'lelaki'
 
 // console.log(siswa)
 // console.log(siswa2)
-
-
-// contoh array of object
-var siswaSiswa = [
-    {nama : 'budi',umur : 40, jenisKelamin : 'lelaki'},
-    {nama : 'andi',umur : 20, jenisKelamin : 'lelaki'},
-    {nama : 'seto',umur : 10, jenisKelamin : 'lelaki'},
-    {nama : 'setyo',umur : 20, jenisKelamin : 'lelaki'},
-]
-
 
 
 // Object Class
@@ -66,15 +64,60 @@ var siswa3 = new Siswa('andi',50,'pria')
 
 
 
+// ******** METHOD 
+
+
+class BlogPost {
+    constructor(title,body,author){
+        this.title = title
+        this.bodyPost = body
+        this.writer = author
+        // method ==> properti yang isinya function
+        this.printBlogTitle = function (){
+            console.log(this.title + ' by ' + this.writer)
+        }
+
+        this.changeTitle = function (newTitle){
+            this.title = newTitle
+        }
+    }
+}
+
+
+
+
+
+var post2 = new BlogPost('Tutorial Memasak enak','this is body','chef')
+// post2.printBlogTitle()
+// post2.changeTitle('Tutorial Bikin Kue')
+// post2.printBlogTitle()
+
+
+
+
 // ********* READ
-
-
 var siswa1 = new Siswa('budi',30,'pria')
-console.log(siswa1)
-console.log(siswa1.nama)
-console.log(siswa1.umur)
-console.log(siswa1.jenisKelamin)
-siswa1.nama = 'andi'
-delete siswa1.nama
+// console.log(siswa1)
+// console.log(siswa1.nama)
+// console.log(siswa1.umur)
+// console.log(siswa1.jenisKelamin)
 
-console.log('Nama Saya ' + siswa1.nama + ', umur ' + siswa1.umur + ' tahun, saya seorang ' +  siswa1.jenisKelamin )
+// console.log('Nama Saya ' + siswa1.nama + ', umur ' + siswa1.umur + ' tahun, saya seorang ' +  siswa1.jenisKelamin )
+
+
+
+// ********* UPDATE
+
+var post1 = new BlogPost('Fundamental Javascript','this is body','fikri')
+post1.writer = 'Seto'
+post1.printBlogTitle()
+post1.publishDate = '28-09-2020'
+console.log(post1)
+
+// *********** DELETE
+
+delete post1.publishDate
+post1.writer = null
+console.log(post1)
+
+
